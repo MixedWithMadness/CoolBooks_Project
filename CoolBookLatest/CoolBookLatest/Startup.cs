1bfa6b2ce41faaae81c0fb32e127c5192f5d5a42
trueinam85 <trueinam@yahoo.com>
2017-05-22 10:37:36 +0200

    Initial

---
+++ CoolBookLatest/CoolBookLatest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CoolBookLatest.Startup))]
+namespace CoolBookLatest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
