78a7aeac0a49826dbff643344e9bc999954e5eff
Simon <simonosterlund@hotmail.com>
2017-05-24 14:02:01 +0200

    Began on reviews

---
+++ CoolBookLatest/CoolBookLatest/Models/ReviewViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CoolBookLatest.Models
+{
+    public class Reviews
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public string UserId { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public Nullable<byte> Rating { get; set; }
+        public System.DateTime Created { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
