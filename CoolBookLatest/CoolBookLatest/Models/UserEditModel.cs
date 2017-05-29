e72a9a6fa4d92e9efc5210b1b6c12726b0937ce1
trueinam85 <trueinam@yahoo.com>
2017-05-29 11:52:09 +0200

    Edit user not done

---
+++ CoolBookLatest/CoolBookLatest/Models/UserEditModel.cs
@@ -0,0 +1,31 @@
+﻿using CoolBookLatest.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CoolBookLatest.Models
+{
+    public class UserEditModel
+    {
+        public string UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Gender SelectedGender { get; set; }
+
+        [DisplayName("BirthDate")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public Nullable<System.DateTime> Birthdate { get; set; }
+        public byte[] Picture { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+       // public string Email { get; set; }
+        public string Info { get; set; }
+    }
+}
