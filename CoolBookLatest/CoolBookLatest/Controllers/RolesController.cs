b0dfed4b8b69baeff048c8d62105917444a92ebc
trueinam85 <trueinam@yahoo.com>
2017-05-30 10:36:07 +0200

    enum countries fixing

---
+++ CoolBookLatest/CoolBookLatest/Controllers/RolesController.cs
@@ -0,0 +1,46 @@
+﻿using CoolBookLatest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CoolBookLatest.Controllers
+{
+    public class RolesController : Controller
+    {
+        CoolBooksEntities db = new CoolBooksEntities();
+        // GET: Roles
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+        
+        public async Task<ActionResult> Create()
+        {
+            //RoleModel model = new RoleModel();
+
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> Create(RoleModel model)
+        {
+            //if (ModelState.IsValid)
+
+            //{
+            AspNetRoles role = new AspNetRoles();
+                role.Name = model.Name;
+           // role.Id = db.AspNetRoles.ToList().Count();
+                db.AspNetRoles.Add(role);
+                db.SaveChangesAsync();
+                return View("Index");
+            //}
+            //else
+            //{
+            //    return View();
+            //}
+        }
+    }
+}
--- CoolBookLatest/CoolBookLatest/Models/UserEditModel.cs
+++ CoolBookLatest/CoolBookLatest/Models/UserEditModel.cs
@@ -51,7 +51,7 @@ namespace CoolBookLatest.Models
         public string City { get; set; }
         [StringLength(45, MinimumLength = 2)]
         [RegularExpression("([a-zöåäÖÅÄ]*)", ErrorMessage = "Only Swedish and English alphabets are expected")]
-        public string Country { get; set; }
+        public Enums.Countries Country { get; set; }
        // public string Email { get; set; }
         public string Info { get; set; }
     }
