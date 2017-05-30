e10ca39bf41799c4cf40ee81fb4072340ff27c2d
reza <rezasaeb@outlook.com>
2017-05-30 10:10:03 +0200

    contact page

--- CoolBookLatest/CoolBookLatest/App_Start/BundleConfig.cs
+++ CoolBookLatest/CoolBookLatest/App_Start/BundleConfig.cs
@@ -21,11 +21,14 @@ namespace CoolBookLatest
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/bootstrapValidator.js",
+                      "~/Scripts/respond.js",
+                      "~/Scripts/contact.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css",
+                      "~/Content/site2.css",
                       "~/Content/font-awesome.min.css",
                       "~/Scripts/site.js"
                       ));
